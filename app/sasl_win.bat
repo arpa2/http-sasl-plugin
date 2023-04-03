3401b36c7d807741f89b3b7ecddfa12afca8d4b0
Henri Manson <h.manson@aal-audio.com>
2023-04-03 11:11:18 +0200

    GSSAPI support

--- app/sasl_win.bat
+++ app/sasl_win.bat
@@ -1,38 +1,46 @@
 @echo off
 
-goto java-client
+goto :java-client
 
 :java-client
 echo starting SaslJavaClient > plugin.log
-java -jar target\SaslJavaClient-1.0-SNAPSHOT-jar-with-dependencies.jar 2>> plugin.log
+java -Djava.util.logging.config.file=logging.properties -Djava.security.auth.login.config=jaas-krb5.conf -Djava.security.krb5.conf=arpa2.conf -jar target\SaslJavaClient-1.0-SNAPSHOT-jar-with-dependencies.jar 2>> plugin.log
 goto end
 
 :browser-client
-echo starting browser-client.exe > plugin.log
 
 REM aeneas.vanrein.org
-REM set KIP_REALM=unicorn.demo.arpa2.org
-REM set SASL_CLIENTUSER_LOGIN=demo
-REM set QUICKSASL_PASSPHRASE=sekreet
+set KIP_REALM=unicorn.demo.arpa2.org
+set SASL_CLIENTUSER_LOGIN=demo
+set QUICKSASL_PASSPHRASE=sekreet
 
-REM mansoft.nl
-REM set KIP_REALM=Top secret supporting x-over
-set KIP_REALM=192.168.2.100
-set SASL_CLIENTUSER_LOGIN=henri
+REM local java servlet
+REM set KIP_REALM=192.168.2.100
+REM set SASL_CLIENTUSER_LOGIN=henri
 set QUICKSASL_PASSPHRASE=1234
 
-REM set KIPSERVICE_CLIENT_REALM=arpa2.net
-REM set KIPSERVICE_CLIENTUSER_LOGIN=demo
-REM set KIPSERVICE_CLIENTUSER_ACL=demo+ali
-
-set KIPSERVICE_CLIENTUSER_LOGIN=henri
-set KIPSERVICE_CLIENTUSER_ACL=henri
+set KIPSERVICE_CLIENTUSER_LOGIN=demo
+set KIPSERVICE_CLIENTUSER_ACL=demo+ali
 set KIPSERVICE_CLIENT_REALM=unicorn.demo.arpa2.org
 
+REM set KIPSERVICE_CLIENTUSER_LOGIN=henri
+REM set KIPSERVICE_CLIENTUSER_ACL=henri
+REM set KIPSERVICE_CLIENT_REALM=unicorn.demo.arpa2.org
+
+set SASL_REALM=192.168.2.100
 set SASL_CLIENT_REALM=arpa2.net
-set SASL_CLIENTUSER_ACL=demo
+set SASL_CLIENTUSER_ACL=henri
 set SASL_PATH=C:\msys64\mingw64\lib\sasl2
 set SASL_CONF_PATH=c:\Users\hfman\Documents\arpa2\http-sasl-plugin\app
 
-browser-client.exe 2>> plugin.log
+set HTTP_SERVICE_NAME=HTTP
+REM set HTTP_SERVICE_NAME=RealmCrossover
+
+set PATH=c:\msys64\mingw64\bin;c:\msys64\mingw64\local\bin;c:\Qt\5.15.2\mingw81_64\bin
+echo starting browser-client.exe > plugin.log
+REM c:\msys64\mingw64\local\bin\browser-client.exe 2>> plugin.log
+REM echo starting sasl-login.exe > plugin.log
+C:\Users\hfman\Documents\Qt\http_sasl_client\build\native\sasl-login.exe 2>> plugin.log
+REM c:\Users\hfman\Documents\Qt\build-http_sasl_client-ARPA2_Desktop_Qt_5_15_2_MinGW_64_bit-Debug\native\sasl-login.exe 2>> plugin.log
+
 :end
--- app/src/main/java/nl/mansoft/sasl/Client.java
+++ app/src/main/java/nl/mansoft/sasl/Client.java
@@ -6,6 +6,7 @@ import javax.security.sasl.SaslClient;
 import java.util.Base64;
 import javax.security.sasl.Sasl;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.Arrays;
@@ -23,6 +24,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ButtonBar;
 import java.io.PrintWriter;
+import java.security.Principal;
 import javax.json.JsonValue;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonString;
@@ -35,6 +37,17 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Dialog;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import javax.security.sasl.SaslException;
 
 /**
  *
@@ -72,19 +85,58 @@ public class Client extends Application {
     private String password;
     private String mechanism;
     private ChoiceBox mechanismsBox;
+    private LoginContext context;
 
-    public void processCallbacks(final Callback[] callbacks) {
-        for (final Callback callback : callbacks) {
-            if (callback instanceof NameCallback) {
-                final NameCallback nameCallback = (NameCallback) callback;
-                nameCallback.setName(nameCallback.getDefaultName());
-            } else if (callback instanceof PasswordCallback) {
-                final PasswordCallback passwordCallback = (PasswordCallback) callback;
-                passwordCallback.setPassword(password.toCharArray());
-            } else if (callback instanceof RealmCallback) {
-                final RealmCallback realmCallback = (RealmCallback) callback;
-                System.err.println("RealmCallback: " + realmCallback.getDefaultText());
-                realmCallback.setText(realmCallback.getDefaultText());
+    private static final String getHexBytes(byte[] bytes, int pos, int len) {
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = pos; i < (pos+len); i++) {
+
+            int b1 = (bytes[i]>>4) & 0x0f;
+            int b2 = bytes[i] & 0x0f;
+
+            sb.append(Integer.toHexString(b1));
+            sb.append(Integer.toHexString(b2));
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    private static final String getHexBytes(byte[] bytes) {
+        return getHexBytes(bytes, 0, bytes.length);
+    }
+
+    public static String bytesToString(byte[] bytes) {
+        boolean allAscii = true;
+        for (byte b : bytes) {
+            if (b < 0x20) {
+                allAscii = false;
+                break;
+            }
+        }
+        return allAscii ? new String(bytes) : getHexBytes(bytes);
+    }
+
+    public static void printBytes(String prompt, byte[] bytes) {
+        System.err.println(prompt + ": " + bytesToString(bytes));
+    }
+
+    class MyCallbackHandler implements CallbackHandler {
+
+        @Override
+        public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+            for (final Callback callback : callbacks) {
+                if (callback instanceof NameCallback) {
+                    final NameCallback nameCallback = (NameCallback) callback;
+                    nameCallback.setName(username);
+                } else if (callback instanceof PasswordCallback) {
+                    final PasswordCallback passwordCallback = (PasswordCallback) callback;
+                    passwordCallback.setPassword(password.toCharArray());
+                } else if (callback instanceof RealmCallback) {
+                    final RealmCallback realmCallback = (RealmCallback) callback;
+                    System.err.println("RealmCallback: " + realmCallback.getDefaultText());
+                    realmCallback.setText(realmCallback.getDefaultText());
+                }
             }
         }
     }
@@ -166,6 +218,61 @@ public class Client extends Application {
         });
     }
 
+    SaslClient createSaslClient(JsonObjectBuilder outputJsonBuilder, String mechanism, String serverName) throws SaslException {
+        SaslClient sc = Sasl.createSaslClient(new String[] { mechanism }, null, "HTTP", serverName, null, new MyCallbackHandler());
+        if (sc.hasInitialResponse()) {
+            System.err.println("hasInitialResponse");
+            final byte[] challenge = new byte[] { };
+            final byte[] response = sc.evaluateChallenge(challenge);
+            if (response == null) {
+                System.err.println("response is null");
+            } else {
+                printBytes("response", response);
+                final String c2s = Base64.getEncoder().encodeToString(response);
+                outputJsonBuilder.add("c2s", c2s);
+            }
+        }
+        return sc;
+    }
+
+    class CreateSaslClient implements PrivilegedExceptionAction<SaslClient> {
+        private final JsonObjectBuilder outputJsonBuilder;
+        private final String mechanism;
+        private final String serverName;
+
+        CreateSaslClient(JsonObjectBuilder outputJsonBuilder, String mechanism, String serverName) {
+            this.outputJsonBuilder = outputJsonBuilder;
+            this.mechanism = mechanism;
+            this.serverName = serverName;
+        }
+
+        @Override
+        public SaslClient run() throws Exception {
+            return createSaslClient(outputJsonBuilder, mechanism, serverName);
+        }
+    }
+
+    class EvaluateChallenge implements PrivilegedExceptionAction<byte[]>{
+        private final SaslClient saslClient;
+        private final byte[] challenge;
+
+        public EvaluateChallenge(SaslClient saslClient, byte[] challenge) {
+            this.saslClient = saslClient;
+            this.challenge = challenge;
+        }
+
+        //saslServer.evaluateResponse
+        @Override
+        public byte[] run() throws SaslException {
+            return saslClient.evaluateChallenge(challenge);
+        }
+    }
+
+    private Subject getSubject() {
+        return context == null ? null : context.getSubject();
+    }
+
+
     @Override
     public void start(final Stage primaryStage) {
         try {
@@ -178,6 +285,7 @@ public class Client extends Application {
                 System.err.println(inputJson);
                 final JsonBuilderFactory factory = Json.createBuilderFactory((Map) null);
                 final JsonObjectBuilder outputJsonBuilder = factory.createObjectBuilder();
+                addString(outputJsonBuilder, inputJson, "requestId");
                 final String mech = jsonGetString(inputJson, "mech");
                 if (mech != null) {
                     System.err.println("creating SASL client, mechanisms: " + mech);
@@ -215,24 +323,41 @@ public class Client extends Application {
                     } else {
                         System.err.println("extraInfoSpec not found");
                     }
-                    final String[] mechanisms = {mechanism};
-                    sc = Sasl.createSaslClient(mechanisms, username, "http", serverName, null, (final Callback[] callbacks) -> {
-                        Client.this.processCallbacks(callbacks);
-                    });
+                    if (mechanism.equals("GSSAPI")) {
+                        try {
+                            context = new LoginContext("client", new MyCallbackHandler());
+                            context.login();
+                            String principals = "Authenticated principals: ";
+                            for (Principal principal : context.getSubject().getPrincipals()) {
+                                principals += principal.getName() + " ";
+                            }
+                            System.err.println(principals);
+                        } catch (LoginException ex) {
+                            System.err.println("LoginException");
+                            context = null;
+                            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, ex.getMessage());
+                            outputJsonBuilder.add("extraInfoSpec", JsonValue.EMPTY_JSON_OBJECT);
+                            final JsonObject outputJson = outputJsonBuilder.build();
+                            nativeMessaging.writeMessage(outputJson);
+                            final String output = outputJson.toString();
+                            System.err.println(output);
+                            continue;
+                        }
+                    }
+                    sc = Subject.doAs(getSubject(), new CreateSaslClient(outputJsonBuilder, mechanism, serverName));
                     outputJsonBuilder.add("mech", sc.getMechanismName());
                 }
-                addString(outputJsonBuilder, inputJson, "requestId");
                 addObject(outputJsonBuilder, inputJson, "extraInfoSpec");
                 addString(outputJsonBuilder, inputJson, "s2s");
                 final String s2cBase64 = jsonGetString(inputJson, "s2c");
                 if (sc != null && s2cBase64 != null) {
                     final byte[] challenge = Base64.getDecoder().decode(s2cBase64);
-                    System.err.println(new String(challenge));
-                    final byte[] response = sc.evaluateChallenge(challenge);
+                    printBytes("challenge", challenge);
+                    final byte[] response = Subject.doAs(getSubject(), new EvaluateChallenge(sc, challenge));
                     if (response == null) {
                         System.err.println("response is null");
                     } else {
-                        System.err.println(new String(response));
+                        printBytes("response", response);
                         final String c2s = Base64.getEncoder().encodeToString(response);
                         outputJsonBuilder.add("c2s", c2s);
                     }
@@ -240,6 +365,15 @@ public class Client extends Application {
                         System.err.println("sc.isComplete()");
                         sc.dispose();
                         sc = null;
+                        if (context != null) {
+                            System.err.println("logging out");
+                            try {
+                                context.logout();
+                            } catch (LoginException ex) {
+                                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            context = null;
+                        }
                     }
                 }
                 final JsonObject outputJson = outputJsonBuilder.build();
@@ -250,6 +384,8 @@ public class Client extends Application {
             System.err.println("EXITING");
         } catch (IOException e) {
             e.printStackTrace(System.err);
+        } catch (PrivilegedActionException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
